a7901b19f44989af7bcfca02d76e3d5be99cde7f
quokka-eating-carrots <ovzip7@gmail.com>
2022-10-15 23:04:39 +0900

    feat: swiper
    JS를 사용해서 swiper 설정
    pagination을 조정하려고 했는데 잘 되지 않았음
    이미지 파일 추가
    review 부분 레이아웃 잡기 위해 작성
    main-menu fixed 값 넣었더니 위치 변경되어서 수정 예정

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
 import Swiper from 'swiper/swiper-bundle';
 import 'swiper/swiper.min.css';
+import 'swiper/swiper-bundle.css';
 
 import gsap from 'gsap';
 
 // SWIPER
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.season--banner .swiper', {
   direction: 'horizontal',
-  autoplay: {
-    delay: 5000
-  },
+  // autoplay: {
+  //   delay: 5000
+  // },
   loop: true,
   slidesPerView: 2,
   spaceBetween: 10,
   centeredSlides: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets'
+  }
 })
 
